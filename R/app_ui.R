da3e22f1ba9d590ab90ecfe781a10969769d7f27
Alexander Alekseyenko <alexander.alekseyenko@gmail.com>
2020-10-06 15:28:14 -0400

    Update app_ui.R

--- R/app_ui.R
+++ R/app_ui.R
@@ -34,7 +34,7 @@ app_ui <- function(request) {
 
   tagList(
     # Leave this function for adding external resources
-    # golem_add_external_resources(),
+    golem_add_external_resources(),
 
     # List the first level UI elements here 
     fluidPage( 
